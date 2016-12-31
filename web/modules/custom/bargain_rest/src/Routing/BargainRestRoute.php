78f81c1a8a5494994e39f307e3a6ac259c298926
Roy Segall <roy.segall@gizra.com>
2016-12-31 19:16:15 +0200

    commit more work.

--- web/modules/custom/bargain_rest/src/Routing/BargainRestRoute.php
+++ web/modules/custom/bargain_rest/src/Routing/BargainRestRoute.php
@@ -2,33 +2,62 @@
 
 namespace Drupal\bargain_rest\Routing;
 
+use Drupal\bargain_rest\Plugin\RestPluginBase;
+use Drupal\Core\Access\AccessResult;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Listens to the dynamic route events.
  */
-class BargainRestRouteSubscriber {
+class BargainRestRoute {
 
   public function routes() {
+
+    $plugins = \Drupal::service('plugin.manager.rest_plugin')->getDefinitions();
     $route_collection = new RouteCollection();
 
-    $route = new Route(
-    // Path to attach this route to:
-      '/example',
-      // Route defaults:
-      array(
-        '_controller' => '\Drupal\example\Controller\ExampleController::content',
-        '_title' => 'Hello'
-      ),
-      // Route requirements:
-      array(
-        '_permission'  => 'access content',
-      )
-    );
-    // Add the route under the name 'example.content'.
-    $route_collection->add('example.content', $route);
+    foreach ($plugins as $plugin) {
+
+      $route = new Route(
+      // Path to attach this route to:
+        $plugin['path'],
+        array(
+          '_controller' => '\Drupal\bargain_rest\Routing\BargainRestRoute::content',
+          '_title' => 'Hello'
+        ),
+        array(
+          '_custom_access'  => '\Drupal\bargain_rest\Routing\BargainRestRoute::access',
+        ),
+        [
+          'plugin' => $plugin,
+        ]
+      );
+
+      // Add the route under the name 'example.content'.
+      $route_collection->add($plugin['id'], $route);
+    }
+
     return $route_collection;
+  }
+
+  public function content() {
+    $plugin_info = \Drupal::routeMatch()->getRouteObject()->getOption('plugin');
+
+    /** @var RestPluginBase $plugin */
+    $plugin = \Drupal::service('plugin.manager.rest_plugin')->createInstance($plugin_info['id']);
+
+    return $plugin->callback();
+  }
+
+  public function access() {
+    $plugin_info = \Drupal::routeMatch()->getRouteObject()->getOption('plugin');
+
+    /** @var RestPluginBase $plugin */
+    $plugin = \Drupal::service('plugin.manager.rest_plugin')->createInstance($plugin_info['id']);
+
+    return $plugin->access();
   }
 
 }
