0264eee8ac9e860de4f29d748660377ff95fc80c
Roy Segall <roy.segall@gizra.com>
2017-01-15 22:15:45 +0200

    Create the exchange rate.

--- web/modules/custom/bargain_core/src/EventSubscriber/BargainCoreEventSubscriber.php
+++ web/modules/custom/bargain_core/src/EventSubscriber/BargainCoreEventSubscriber.php
@@ -10,8 +10,6 @@ namespace Drupal\bargain_core\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\DomCrawler\Crawler;
-
 
 class BargainCoreEventSubscriber implements EventSubscriberInterface {
 
@@ -19,25 +17,7 @@ class BargainCoreEventSubscriber implements EventSubscriberInterface {
    * Initializes bargain core module requirements.
    */
   public function onRequest(GetResponseEvent $event) {
-    $source = \Drupal::config('bargain_core.database')->get('currency_source');
-
-    $request = \Drupal::httpClient()->get($source);
-    $crawler = new Crawler($request->getBody(true)->getContents());
-    $results = [];
-
-    $crawler->filterXPath('//CURRENCIES//CURRENCY')->each(function (Crawler $node, $i) use(&$results) {
-      $node = $node->getNode(0);
-
-      $item = [];
-      foreach (['NAME', 'UNIT', 'CURRENCYCODE', 'COUNTRY', 'RATE', 'CHANGE'] as $key) {
-        $item[$key] = $node->getElementsByTagName($key)->item(0)->nodeValue;
-      }
-
-      $results[] = $item;
-
-    });
-
-    $date = $crawler->filterXPath('//CURRENCIES//LAST_UPDATE')->text();
+    \Drupal::service('bargain_exchange_rate.pull_exchange_rate')->pull();
   }
 
   /**
