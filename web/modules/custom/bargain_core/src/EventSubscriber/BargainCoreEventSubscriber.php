3132e7534a79bf481f3215cdd534f426f398629d
Roy Segall <roy.segall@gizra.com>
2017-01-15 19:49:38 +0200

    Pull the data from the bank.

--- web/modules/custom/bargain_core/src/EventSubscriber/BargainCoreEventSubscriber.php
+++ web/modules/custom/bargain_core/src/EventSubscriber/BargainCoreEventSubscriber.php
@@ -10,6 +10,8 @@ namespace Drupal\bargain_core\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DomCrawler\Crawler;
+
 
 class BargainCoreEventSubscriber implements EventSubscriberInterface {
 
@@ -17,6 +19,21 @@ class BargainCoreEventSubscriber implements EventSubscriberInterface {
    * Initializes bargain core module requirements.
    */
   public function onRequest(GetResponseEvent $event) {
+    $request = \Drupal::httpClient()->get('http://www.boi.org.il/currency.xml');
+    $crawler = new Crawler($request->getBody(true)->getContents());
+    $results = [];
+
+    $crawler->filterXPath('//CURRENCIES//CURRENCY')->each(function (Crawler $node, $i) use(&$results) {
+      $node = $node->getNode(0);
+
+      $item = [];
+      foreach (['NAME', 'UNIT', 'CURRENCYCODE', 'COUNTRY', 'RATE', 'CHANGE'] as $key) {
+        $item[$key] = $node->getElementsByTagName($key)->item(0)->nodeValue;
+      }
+
+      $results[] = $item;
+
+    });
   }
 
   /**
