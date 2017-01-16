f346d8b2c63e9274104cf6022c0364fd473db958
Roy Segall <roy.segall@gizra.com>
2017-01-16 12:25:59 +0200

    #5: Adding access token.

---
+++ web/modules/custom/bargain_rest/src/Plugin/RestPlugin/RestUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\bargain_rest\Plugin\RestPlugin;
+
+use Drupal\bargain_core\BargainCore;
+use Drupal\bargain_rest\Plugin\RestPluginBase;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * @RestPlugin(
+ *  id = "rest_user",
+ *  path = "/rest_user",
+ *  label = @Translation("Rest user"),
+ *  description = @Translation("The user in the rest request")
+ * )
+ */
+class RestUser extends RestPluginBase {
+
+  protected $callbacks = [
+    'get' => 'get',
+  ];
+
+  /**
+   * Return access callback for the routes.
+   *
+   * @return AccessResult
+   */
+  public function access() {
+    return AccessResult::allowed();
+  }
+
+  /**
+   * Get callback; Return list of plugins.
+   */
+  protected function get() {
+    $account = $this->entityTypeManager->getStorage('user')->load($this->accountProxy->id());
+    return $this->entityFlatten->flatten($account);
+  }
+
+}
--- web/modules/custom/bargain_rest/src/Plugin/RestPluginBase.php
+++ web/modules/custom/bargain_rest/src/Plugin/RestPluginBase.php
@@ -2,11 +2,15 @@
 
 namespace Drupal\bargain_rest\Plugin;
 
+use Drupal\bargain_core\BargainCoreEntityFlatten;
 use Drupal\Component\Plugin\PluginBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxy;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Base class for Rest plugin plugins.
@@ -16,21 +20,35 @@ abstract class RestPluginBase extends PluginBase implements RestPluginInterface,
   /**
    * The request object.
    *
-   * @var \Symfony\Component\HttpFoundation\Request
+   * @var Request
    */
   protected $request;
 
   /**
    * The plugin manager instance.
    *
-   * @var \Drupal\bargain_rest\Plugin\RestPluginManager
+   * @var RestPluginManager
    */
   protected $pluginManager;
 
   /**
-   * @var \Drupal\Core\Session\AccountProxy
+   * @var AccountProxy
    */
   protected $accountProxy;
+
+  /**
+   * Drupal\Core\Entity\EntityManager definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Entity flatten service.
+   *
+   * @var \Drupal\bargain_core\BargainCoreEntityFlatten
+   */
+  protected $entityFlatten;
 
   /**
    * List of callbacks.
@@ -52,16 +70,24 @@ abstract class RestPluginBase extends PluginBase implements RestPluginInterface,
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @param Request $request
    *   The request object.
    * @param RestPluginManager $plugin_manager
    *   The rest plugin manager.
+   * @param \Drupal\Core\Session\AccountProxy $account_proxy
+   *   The account proxy.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   The entity type manager.
+   * @param \Drupal\bargain_core\BargainCoreEntityFlatten $entity_flatten
+   *   The entity flatten service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, \Symfony\Component\HttpFoundation\Request $request, \Drupal\bargain_rest\Plugin\RestPluginManager $plugin_manager, \Drupal\Core\Session\AccountProxy $account_proxy) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Request $request, RestPluginManager $plugin_manager, AccountProxy $account_proxy, EntityTypeManagerInterface $entity_manager, BargainCoreEntityFlatten $entity_flatten) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->request = $request;
     $this->pluginManager = $plugin_manager;
     $this->accountProxy = $account_proxy;
+    $this->entityTypeManager = $entity_manager;
+    $this->entityFlatten = $entity_flatten;
   }
 
   /**
@@ -74,7 +100,9 @@ abstract class RestPluginBase extends PluginBase implements RestPluginInterface,
       $plugin_definition,
       $container->get('request_stack')->getCurrentRequest(),
       $container->get('plugin.manager.rest_plugin'),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('entity_type.manager'),
+      $container->get('bargain_core.entity_flatter')
     );
   }
 
