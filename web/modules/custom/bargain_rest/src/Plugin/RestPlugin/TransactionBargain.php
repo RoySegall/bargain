6f1a7a0711ecc03d3030bca54604d3de79801064
Roy Segall <roy.segall@gizra.com>
2017-01-16 14:24:30 +0200

    Improving the rest plugin logic.

---
+++ web/modules/custom/bargain_rest/src/Plugin/RestPlugin/TransactionBargain.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\bargain_rest\Plugin\RestPlugin;
+
+use Drupal\bargain_rest\Plugin\RestPluginBase;
+use Drupal\bargain_transaction\Entity\BargainTransaction;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * @RestPlugin(
+ *  id = "rest_user",
+ *  path = "/transaction/{bargain_transaction}/foo/{bar}",
+ *  label = @Translation("Transaction list"),
+ *  description = @Translation("A single transaction")
+ * )
+ */
+class TransactionBargain extends RestPluginBase {
+
+  protected $callbacks = [
+    'get' => 'get',
+  ];
+
+  /**
+   * Return access callback for the routes.
+   *
+   * @return AccessResult
+   */
+  public function access() {
+    return AccessResult::allowed();
+  }
+
+  /**
+   * Get callback; Return list of plugins.
+   *
+   * @param \Drupal\bargain_transaction\Entity\BargainTransaction $transaction
+   *   The entity object.
+   *
+   * @return mixed
+   *   The page.
+   */
+  protected function get(BargainTransaction $transaction, $bar) {
+    return $transaction->label();
+  }
+
+}
