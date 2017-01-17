e941533130178465b8a2323ed5b3e64259aaf50f
Roy Segall <roy.segall@gizra.com>
2017-01-17 22:29:15 +0200

    Maybe a fix.

---
+++ scripts/travis/run-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run either PHPUnit tests or PHP_CodeSniffer tests on Travis CI, depending
+# on the passed in parameter.
+
+case "$1" in
+    cs)
+        ./vendor/bin/phpcs
+        exit $?
+        ;;
+    kernal)
+        ./../vendor/bin/drush site-install --verbose --yes --db-url=sqlite://tmp/site.sqlite;
+        ./../vendor/bin/drush runserver http://127.0.0.1:8080 &
+        echo "kernal";
+#        exit $?
+esac
