cd41c486d73cff9773b0ea395f2706e58c28507f
Roy Segall <roy.segall@gizra.com>
2017-01-19 23:36:55 +0200

    Another fix.

--- scripts/travis/unit.sh
+++ scripts/travis/unit.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-CORE_DIRECTORY=$(pwd)
 cd web
 ./../vendor/bin/drush site-install --verbose --yes --db-url=sqlite://tmp/site.sqlite
-cd $CORE_DIRECTORY
-./vendor/bin/phpunit -c ${PWD}/web/core/phpunit.xml.dist web/modules/custom/bargain_core/tests/
+./../vendor/bin/phpunit -c web/core/phpunit.xml.dist web/modules/custom/bargain_core/tests/
