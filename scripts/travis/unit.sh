bf2f24336cbfda66781195185cb4fe9247e8ec64
Roy Segall <roy.segall@gizra.com>
2017-01-19 23:41:58 +0200

    Locations.

--- scripts/travis/unit.sh
+++ scripts/travis/unit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd web
 ./../vendor/bin/drush site-install --verbose --yes --db-url=sqlite://tmp/site.sqlite
-./../vendor/bin/phpunit -c web/core/phpunit.xml.dist web/modules/custom/bargain_core/tests/
+./../vendor/bin/phpunit -c core/phpunit.xml.dist web/modules/custom
