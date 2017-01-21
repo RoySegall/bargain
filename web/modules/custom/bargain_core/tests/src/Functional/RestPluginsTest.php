2d7170a99f9b77e08881ea447d010dce1e3549d8
Roy Segall <roy.segall@gizra.com>
2017-01-21 23:18:43 +0200

    Adding tests for rest plugins

---
+++ web/modules/custom/bargain_core/tests/src/Functional/RestPluginsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\bargain_core\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Testing Rest plugins.
+ *
+ * @group bargain
+ */
+class RestPluginsTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'bargain_rest',
+    'bargain_core',
+  ];
+
+  /**
+   * The entity type manager interface.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The HTTP client service.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+  /**
+   * The JSON service.
+   *
+   * @var \Drupal\Component\Serialization\Json
+   */
+  protected $json;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->httpClient = $this->container->get('http_client');
+    $this->json = $this->container->get('serialization.json');
+  }
+
+  /**
+   * Testing the /api rest plugin.
+   */
+  public function testRestPlugins() {
+    $results = $this->json->decode($this->httpClient->request('get', $this->baseUrl . '/api')->getBody()->getContents());
+    $this->assertTrue(key_exists('transaction_bargain', $results));
+    $this->assertTrue(key_exists('rest_user', $results));
+    $this->assertTrue(key_exists('rest_plugin', $results));
+  }
+
+}
