1984850e2b509ce1a6e40cd1eaaf01e7b65c80c6
Roy Segall <roy.segall@gizra.com>
2017-01-22 22:38:58 +0200

    Maybe now this will work.

--- scripts/travis/unit.sh
+++ scripts/travis/unit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 CORE_DIRECTORY=$(pwd)
 cd web
-./../vendor/bin/drush site-install --verbose --yes --db-url=sqlite://tmp/site.sqlite
+./../vendor/bin/drush site-install --verbose --yes --db-url=mysql://root:@127.0.0.1/bargain
 cd $CORE_DIRECTORY
 ./vendor/bin/phpunit -c web/core/phpunit.xml.dist web/modules/custom
