2f71f7e8dc74ba4bb9d78c31b3ae283a16d0aafb
Roy Segall <roy.segall@gizra.com>
2017-01-22 23:18:23 +0200

    The server never run?

--- scripts/travis/unit.sh
+++ scripts/travis/unit.sh
@@ -2,5 +2,6 @@
 CORE_DIRECTORY=$(pwd)
 cd web
 ./../vendor/bin/drush site-install --verbose --yes --db-url=mysql://root:@127.0.0.1/bargain
+php -S localhost:8888 &
 cd $CORE_DIRECTORY
 ./vendor/bin/phpunit -c web/core/phpunit.xml.dist web/modules/custom
