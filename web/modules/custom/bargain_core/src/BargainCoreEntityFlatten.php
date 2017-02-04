b3fc1e31f0d3d873ad0732bb140006add5d66c3e
Roy Segall <roy.segall@gizra.com>
2017-02-04 23:34:10 +0200

    Adding list of available rooms.

--- web/modules/custom/bargain_core/src/BargainCoreEntityFlatten.php
+++ web/modules/custom/bargain_core/src/BargainCoreEntityFlatten.php
@@ -15,11 +15,20 @@ class BargainCoreEntityFlatten {
    * @param EntityInterface $entity
    *   The entity object.
    *
+   * @param array $entity_reference_handlers
+   *   List of entity reference handlers. The array should be in the form of:
+   *    [
+   *      'field' => function($item) {
+   *        return 'new_value';
+   *      }
+   *    ]
+   *
    * @return array
    *   The entity in a JSON representation.
    */
-  public function flatten(EntityInterface $entity) {
+  public function flatten(EntityInterface $entity, $entity_reference_handlers = []) {
     $fields = $entity->toArray();
+    $definitions = $entity->getFieldDefinitions();
 
     $return = [];
     foreach ($fields as $field => $value) {
@@ -28,9 +37,15 @@ class BargainCoreEntityFlatten {
         continue;
       }
 
-      $value = array_map(function ($item) {
-        return reset($item);
-      }, $value);
+      if ($definitions[$field]->getType() == 'entity_reference' && !empty($entity_reference_handlers[$field])) {
+        $value = $entity_reference_handlers[$field]($entity->get($field)->referencedEntities());
+      }
+      else {
+        $value = array_map(function ($item) {
+          return reset($item);
+        }, $value);
+      }
+
 
       $return[$field] = count($value) == 1 ? reset($value) : $value;
     }
---
+++ web/modules/custom/bargain_rest/src/Plugin/RestPlugin/ChatRoomsRest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\bargain_rest\Plugin\RestPlugin;
+
+use Drupal\bargain_rest\Plugin\RestPluginBase;
+use Drupal\Core\Access\AccessResult;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * ChatRoomsRest class.
+ *
+ * @RestPlugin(
+ *  id = "chat_rooms_rest",
+ *  path = "/messages",
+ *  label = @Translation("Chat rooms"),
+ *  description = @Translation("Displat list of the rooms which the user can access")
+ * )
+ */
+class ChatRoomsRest extends RestPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $callbacks = [
+    'get' => 'entityQuery',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access() {
+
+    switch ($this->requestType) {
+      case 'get':
+        return AccessResult::allowedIf($this->getRoomsForUsers(TRUE) > 0);
+    }
+
+    throw new BadRequestHttpException('This end point does not support the request type.');
+  }
+
+  /**
+   * Get the list of rooms which the user can access to.
+   *
+   * @param bool $count
+   *   Check if the query need to return a number or rooms or list of entities.
+   *
+   * @return string
+   *   JSON which represent the list of rooms.
+   */
+  protected function getRoomsForUsers($count = FALSE) {
+    $query = $this
+      ->entityQuery
+      ->get('bargain_chat_room', 'OR')
+      ->condition('user_id', $this->accountProxy->id())
+      ->condition('buyer', $this->accountProxy->id());
+
+    if ($count) {
+      return $query->count()->execute();
+    }
+
+    return $query->execute();
+  }
+
+  /**
+   * Return list of queries.
+   *
+   * @return array
+   *   List of entities.
+   */
+  public function entityQuery() {
+    $rooms = $this->getRoomsForUsers();
+
+    $entities = $this
+      ->entityTypeManager
+      ->getStorage('bargain_chat_room')
+      ->loadMultiple($rooms);
+
+    $return = [];
+
+    foreach ($entities as $entity) {
+      $return[] = $this->entityFlatten->flatten($entity, [
+        'buyer' => function ($entities) {
+          return $this->getReferencedUser($entities);
+        },
+        'user_id' => function ($entities) {
+          return $this->getReferencedUser($entities);
+        },
+      ]);
+    }
+
+    return $return;
+  }
+
+  /**
+   * Get the label and ID of referenced users from an entity reference field.
+   *
+   * @param \Drupal\user\Entity\User[] $entities
+   *   List of items.
+   *
+   * @return array
+   *   List of ids and labels.
+   */
+  protected function getReferencedUser($entities) {
+    $return = [];
+    foreach ($entities as $entity) {
+      $return[] = ['id' => $entity->id(), 'label' => $entity->label()];
+    }
+
+    return $return;
+  }
+
+}
