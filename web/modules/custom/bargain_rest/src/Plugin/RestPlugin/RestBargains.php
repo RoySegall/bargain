13e4921874092543a935ce6c83cee8d1e0e1f38b
Roy Segall <roy.segall@gizra.com>
2017-02-18 21:47:49 +0200

    Damm you gitx.

--- web/modules/custom/bargain_rest/src/Plugin/RestPlugin/RestBargains.php
+++ web/modules/custom/bargain_rest/src/Plugin/RestPlugin/RestBargains.php
@@ -2,116 +2,67 @@
 
 namespace Drupal\bargain_rest\Plugin\RestPlugin;
 
-use Drupal\bargain_chat\Entity\BargainChatRoom;
 use Drupal\bargain_rest\Plugin\RestPluginBase;
 use Drupal\Core\Access\AccessResult;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
- * ChatRoomMessagesRest class.
+ * RestBargains class.
  *
  * @RestPlugin(
- *  id = "chat_room_message_rest",
- *  path = "/messages/{bargain_chat_room}",
- *  label = @Translation("Chat room message"),
- *  description = @Translation("Display all the messages in the current room."),
- *  entity_type = "bargain_chat_message"
+ *  id = "rest_bargains",
+ *  path = "/bargains/{type}",
+ *  label = @Translation("Rest bargains"),
+ *  description = @Translation("Display all the bargains."),
+ *  entity_type = "bargain_transaction"
  * )
  */
-class ChatRoomMessagesRest extends RestPluginBase {
+class RestBargains extends RestPluginBase {
 
   /**
    * {@inheritdoc}
    */
   protected $callbacks = [
     'get' => 'entityQuery',
-    'post' => 'entityCreate',
-    'patch' => 'messagePatch',
   ];
 
   /**
    * {@inheritdoc}
    */
   public function access() {
-
-    if ($this->getAccount()->hasPermission('administer bargain chat room entities')) {
-      return AccessResult::allowed();
-    }
-
-    $entity = $this->arguments[0];
-    return AccessResult::allowedIf(in_array($this->accountProxy->id(), [
-      $entity->get('user_id')->referencedEntities()[0]->id(),
-      $entity->get('buyer')->referencedEntities()[0]->id(),
-    ]));
+    return AccessResult::allowed();
   }
 
   /**
    * Return list of entities.
    *
-   * @param BargainChatRoom $entity
-   *   The room object.
+   * @param string $type
+   *   The bargain type.
    *
    * @return array
    *   List of entities.
    */
-  public function entityQuery(BargainChatRoom $entity) {
+  public function entityQuery($type) {
     $results = $this
       ->entityQuery
-      ->get('bargain_chat_message')
-      ->condition('room', $entity->id())
+      ->get('bargain_transaction')
+      ->condition('type', $type)
       ->execute();
 
     if (!$results) {
       return [];
     }
 
-    $messages = $this
+    $bargains = $this
       ->entityTypeManager
-      ->getStorage('bargain_chat_message')
+      ->getStorage('bargain_transaction')
       ->loadMultiple($results);
 
     $return = [];
-    foreach ($messages as $message) {
-      $return[] = $this->entityFlatten->flatten($message, [
-        'user_id' => function ($entities) {
-          return $this->getReferencedUser($entities);
-        },
-      ]);
+    foreach ($bargains as $bargain) {
+      $return[] = $this->entityFlatten->flatten($bargain);
     }
 
     return $return;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function entityCreate() {
-    $this->payload['room'] = $this->arguments[0]->id();
-    $this->payload['status'] = 'sent';
-    return parent::entityCreate();
-  }
-
-  /**
-   * Change the status property of the message.
-   */
-  public function messagePatch() {
-    $message = $this->entityTypeManager->getStorage('bargain_chat_message')->loadByProperties([
-      'uuid' => $this->payload['message_uuid'],
-      'room' => $this->arguments[0]->id(),
-    ]);
-
-    if (!$message) {
-      throw new BadRequestHttpException('There is no message which belong to this room or with the give uuid.');
-    }
-
-    $entity = reset($message);
-    $entity->set('status', $this->payload['status']);
-    $entity->save();
-
-    $this->entityValidate($entity);
-    $entity->save();
-
-    return $this->entityFlatten->flatten($entity);
   }
 
 }
